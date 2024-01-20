f66ae53752b563d3b5ea4603890e98f03ba75e2f
denvt <denvtapkah@yandex.ru>
2024-01-20 20:52:32 +0400

    demo-version

--- src/main/java/com/example/demo/config/MvcConfig.java
+++ src/main/java/com/example/demo/config/MvcConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,9 +2,6 @@ package com.example.demo.repository;
 
 import com.example.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import javax.crypto.spec.OAEPParameterSpec;
 import java.util.List;
 import java.util.Optional;
 
