202560cd0a2ba47d808d14ad0622596befe6f65b
denvt <denvtapkah@yandex.ru>
2024-01-25 20:47:16 +0400

    алфьа

--- src/main/resources/static/js/myCourses.js
+++ src/main/resources/static/js/myCourses.js
@@ -8,12 +8,15 @@ async function getMyCourses(){
     let courses = await response.json();
     let list = document.querySelector('.myCourses');
     console.log(courses);
+    let name;
     for(key in courses){
+
+        name = courses[key].name.split('.')[0];
         list.innerHTML += `
             <div class="col search-card w-100" data-title="${courses[key].name}">
                                        <div class="card row">
                                            <div class="card-body row">
-                                               <h5 class="card-title col-10">${courses[key].name}</h5>
+                                               <h5 class="card-title col-10">${name}</h5>
                                                <a class="btn btn-primary col" href="/files/${courses[key].id}">Скачать</a>
                                            </div>
                                        </div>
